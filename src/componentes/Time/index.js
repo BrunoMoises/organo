9c350377dbe85fc73a41984fa7865dcb17015ccc
Bruno <bruno_moises_stz@hotmail.com>
2023-02-21 14:06:30 -0300

    Finalizando opção de alterar cor do time

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,22 +1,24 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
+import hexToRgba from 'hex-to-rgba';
 
 const Time = (props) => {
-const css = { backgroundColor: props.corSecundaria }
+    const css = { backgroundColor: hexToRgba(props.cor, '0.6'), backgroundImage: 'url(/imagens/fundo.png)' }
 
     return (
         props.colaboradores.length > 0 && <section className='time' style={css}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <input value={props.corSecundaria} onChange={e => props.mudarCor(e.target.value, props.nome)} type='color' className='input-cor' />
+            <h3 style={{ borderColor: props.cor }}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map(colaborador => {
-                    return <Colaborador 
-                                corDeFundo={props.corPrimaria} 
-                                key={colaborador.nome} 
-                                nome={colaborador.nome} 
-                                cargo={colaborador.cargo} 
-                                imagem={colaborador.imagem} 
-                                aoDeletar={props.aoDeletar}
-                            />;
+                    return <Colaborador
+                        corDeFundo={props.cor}
+                        key={colaborador.nome}
+                        nome={colaborador.nome}
+                        cargo={colaborador.cargo}
+                        imagem={colaborador.imagem}
+                        aoDeletar={props.aoDeletar}
+                    />;
                 })}
             </div>
         </section>
